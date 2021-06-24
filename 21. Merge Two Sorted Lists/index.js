19f7474ebb01c41ab7cd24d3bdc24ac06f029f27
bruceyee07 <yehao402@163.com>
2021-06-24 23:37:46 +0800

    merge-two-sorted-lists refactor

--- 21. Merge Two Sorted Lists/index.js
+++ 21. Merge Two Sorted Lists/index.js
@@ -57,3 +57,8 @@ function arrayToLinkedList(arr) {
   }
   return head;
 }
+
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val);
+  this.next = (next === undefined ? null : next);
+}
