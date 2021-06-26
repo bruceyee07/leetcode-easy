a3f972cca84b73df4b08062c852c45726aed960b
bruceyee07 <yehao402@163.com>
2021-06-26 21:01:28 +0800

    remove-linked-list-elements other solution inspired by one python solution

--- 203. Remove Linked List Elements/other.js
+++ 203. Remove Linked List Elements/other.js
@@ -23,3 +23,32 @@ var removeElements = function(head, val) {
   head.next = next;
   return head;
 };
+
+/**
+ * @description https://leetcode.com/problems/remove-linked-list-elements/
+ * @tutorial https://leetcode.com/problems/remove-linked-list-elements/discuss/158651/Simple-Python-solution-with-explanation-(single-pointer-dummy-head).
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+  var dummy = new ListNode(-1);
+  dummy.next = head;
+
+  var node = dummy;
+
+  while (node.next) {
+    if (node.next.val === val) {
+      node.next = node.next.next
+    } else {
+      node = node.next;
+    }
+  }
+
+  return dummy.next;
+};
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
