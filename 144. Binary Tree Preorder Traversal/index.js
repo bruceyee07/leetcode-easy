9d726c1be417ba44f75bdc5e199b8cd39a228949
bruceyee07 <yehao402@163.com>
2021-07-08 22:57:27 +0800

    binary-tree-preorder-traversal recursive javascript solution

---
+++ 144. Binary Tree Preorder Traversal/index.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @description https://leetcode.com/problems/binary-tree-preorder-traversal/
+ * @type self
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+  if (root === null) {
+    return [];
+  }
+  if (this.left === null && this.right === null) {
+    return [root.val];
+  }
+  return [root.val].concat(preorderTraversal(root.left)).concat(preorderTraversal(root.right));
+};
--- 21. Merge Two Sorted Lists/index.js
+++ 21. Merge Two Sorted Lists/index.js
@@ -57,8 +57,3 @@ function arrayToLinkedList(arr) {
   }
   return head;
 }
-
-function ListNode(val, next) {
-  this.val = (val === undefined ? 0 : val);
-  this.next = (next === undefined ? null : next);
-}
