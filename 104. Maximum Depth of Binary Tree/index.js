2f5b9bfd210c943a8b6f2db125eb11818d2ff939
bruceyee07 <yehao402@163.com>
2021-07-11 22:38:12 +0800

    maximum-depth-of-binary-tree recursive javascript solution

---
+++ 104. Maximum Depth of Binary Tree/index.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @description https://leetcode.com/problems/maximum-depth-of-binary-tree/
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (root === null) {
+    return 0;
+  }
+
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
