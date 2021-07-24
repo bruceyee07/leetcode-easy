c981fc36f3cd182233d0a3d82507ca83c80dd432
bruceyee07 <yehao402@163.com>
2021-07-24 20:46:00 +0800

    minimum-absolute-difference-in-bst using pre-order

---
+++ 530. Minimum Absolute Difference in BST/index.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @description https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+ * @type self
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var getMinimumDifference = function(root) {
+  var inorderRes = inorder(root);
+  let min = Infinity;
+
+  for (let i = 0, j = 1; i < inorderRes.length - 1, j < inorderRes.length; i++, j++) {
+    if (min > inorderRes[j] - inorderRes[i]) {
+      min = inorderRes[j] - inorderRes[i];
+    }
+  }
+
+  return min;
+};
+
+function inorder(root) {
+  if (!root) {
+    return [];
+  }
+  return inorder(root.left).concat([root.val], inorder(root.right));
+}
