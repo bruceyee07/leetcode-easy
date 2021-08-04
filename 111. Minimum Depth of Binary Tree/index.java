e96736d822a804c587a9317f741e6f002028c28e
bruceyee07 <yehao402@163.com>
2021-08-05 01:45:35 +0800

    minimum-depth-of-binary-tree dfs java solution

---
+++ 111. Minimum Depth of Binary Tree/index.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * https://leetcode.com/problems/minimum-depth-of-binary-tree/
+ * self
+ */
+class Solution {
+  public int minDepth(TreeNode root) {
+    if (root == null) return 0;
+    if (root.left == null) return minDepth(root.right) + 1;
+    if (root.right == null) return minDepth(root.left) + 1;
+    return Math.min(minDepth(root.left), minDepth(root.right)) + 1; 
+  }
+}
