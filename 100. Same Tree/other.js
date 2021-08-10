1f554cf030c73907f7eaed2e747e911c55b56cb4
bruceyee07 <yehao402@163.com>
2021-08-11 01:30:08 +0800

    same-tree recursive js solution

---
+++ 100. Same Tree/other.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @description https://leetcode.com/problems/same-tree/
+ * @tutorial https://leetcode.com/problems/same-tree/solution/
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+  if (p === null && q === null) {
+    return true;
+  }
+  if (p === null || q === null) {
+    return false;
+  }
+  if (p.val !== q.val) {
+    return false;
+  }
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
