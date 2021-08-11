569ec665b42e93544d6b5cadd1bef69e9ea072ae
bruceyee07 <yehao402@163.com>
2021-08-11 23:21:12 +0800

    subtree-of-another-tree recursive java solution

---
+++ 572. Subtree of Another Tree/index.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * https://leetcode.com/problems/subtree-of-another-tree/
+ * self
+ */
+class Solution {
+  public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+    if (root == null) return false;
+    return isSameTree(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+  }
+
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    if (p == null && q == null) return true;
+    if (p == null || q == null) return false;
+    if (p.val != q.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  }
+}
