7e9cc45e607bfe4a1425d06cd1b4584b5d5b2e6c
bruceyee07 <yehao402@163.com>
2021-08-13 13:27:35 +0800

    diameter-of-binary-tree dfs js solution refactor

--- 543. Diameter of Binary Tree/other.js
+++ 543. Diameter of Binary Tree/other.js
@@ -30,3 +30,17 @@ function maxDepth(root) {
   if (!root) return 0;
   return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 }
+
+// other version
+var diameterOfBinaryTree = function(root) {
+  var diameter = 0;
+  function maxDepth(root) {
+    if (!root) return 0;
+    var left = maxDepth(root.left);
+    var right = maxDepth(root.right);
+    diameter = Math.max(diameter, left + right);
+    return Math.max(left, right) + 1;
+  }
+  maxDepth(root);
+  return diameter;
+};
