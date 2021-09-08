b7ded3b38fd38654c87f4464786e6808d7bccbbd
bruceyee07 <yehao402@163.com>
2021-09-08 13:40:25 +0800

    find-the-difference java solution using bit manipulation

---
+++ 389. Find the Difference/other.java
@@ -0,0 +1,15 @@
+/**
+ * https://leetcode.com/problems/find-the-difference/
+ * https://leetcode.com/problems/find-the-difference/discuss/86825/Java-solution-using-bit-manipulation
+ */
+class Solution {
+  public char findTheDifference(String s, String t) {
+    int n = t.length();
+    char c = t.charAt(n - 1);
+    for (int i = 0; i < n - 1; i++) {
+      c ^= s.charAt(i);
+      c ^= t.charAt(i);
+    }
+    return c;
+  }
+}
