61ad66366e9c08e00537058aa8f842bda31f2d7b
bruceyee07 <yehao402@163.com>
2021-10-06 00:18:16 +0800

    next-greater-element-i java solution using monotonic stack

---
+++ 496. Next Greater Element I/index.java
@@ -0,0 +1,25 @@
+/**
+ * https://leetcode.com/problems/next-greater-element-i/
+ * self
+ */
+class Solution {
+  public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    int[] ans = new int[nums1.length];
+    Stack<Integer> stack = new Stack<>();
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for (int i = nums2.length - 1; i >= 0; i--) {
+      while (!stack.isEmpty() && nums2[i] >= stack.peek()) {
+        stack.pop();
+      }
+      map.put(nums2[i], stack.isEmpty() ? -1 : stack.peek());
+      stack.push(nums2[i]);
+    }
+
+    for (int i = 0; i < nums1.length; i++) {
+      ans[i] = map.get(nums1[i]);
+    }
+
+    return ans;
+  }
+}
